677ce10f03ee0a47dcbd8eb5af0f1e5ba09c8176
Sapa Denys <sapa.denis@gmail.com>
2015-08-03 17:52:53 +0300

    try to finde device

--- BluetoothChat/BTCConnectViewController.m
+++ BluetoothChat/BTCConnectViewController.m
@@ -12,7 +12,7 @@
 
 static NSString *const kServiceType = @"sapa-textchat";
 
-@interface BTCConnectViewController () <MCBrowserViewControllerDelegate, MCSessionDelegate, MCNearbyServiceBrowserDelegate>
+@interface BTCConnectViewController () <MCBrowserViewControllerDelegate, MCSessionDelegate, MCNearbyServiceBrowserDelegate, MCNearbyServiceAdvertiserDelegate>
 
 @property (nonatomic, strong) MCSession *currentSession;
 
@@ -28,16 +28,35 @@ static NSString *const kServiceType = @"sapa-textchat";
 
 - (void)viewDidLoad
 {
+	[super viewDidLoad];
+	[self.connect setHidden:NO];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+	[super viewDidAppear:animated];
+	
 	_nearestPeers = [NSMutableArray new];
+	
 	_localPeerID = [[MCPeerID alloc] initWithDisplayName:[[UIDevice currentDevice] name]];
 	
 	_currentSession = [[MCSession alloc] initWithPeer:self.localPeerID
 									 securityIdentity:nil
-								 encryptionPreference:MCEncryptionNone];
-	self.currentSession.delegate = self;
+								 encryptionPreference:MCEncryptionRequired];
 	
-	[super viewDidLoad];
-	[self.connect setHidden:NO];
+	_currentSession.delegate = self;
+	
+	
+	
+	MCNearbyServiceAdvertiser *advertiser = [[MCNearbyServiceAdvertiser alloc] initWithPeer:self.localPeerID discoveryInfo:nil serviceType:kServiceType];
+	advertiser.delegate = self;
+	
+	MCNearbyServiceBrowser *browser = [[MCNearbyServiceBrowser alloc] initWithPeer:self.localPeerID
+																	   serviceType:kServiceType];
+	browser.delegate = self;
+	
+	[advertiser startAdvertisingPeer];
+	[browser startBrowsingForPeers];
 }
 
 - (void)didReceiveMemoryWarning
@@ -54,25 +73,35 @@ static NSString *const kServiceType = @"sapa-textchat";
 																	   serviceType:kServiceType];
 	browser.delegate = self;
 	
-	MCBrowserViewController *browserViewController =
-	[[MCBrowserViewController alloc] initWithBrowser:browser
-											 session:self.currentSession];
+//	MCBrowserViewController *browserViewController = [[MCBrowserViewController alloc] initWithServiceType:kServiceType session:self.currentSession];
+//	[[MCBrowserViewController alloc] initWithBrowser:browser
+//											 session:self.currentSession];
+	[browser startBrowsingForPeers];
+	
+	/*
 	browserViewController.delegate = self;
 	[self presentViewController:browserViewController
 					   animated:YES
 					 completion:^{
-						 [browser startBrowsingForPeers];
+						 
 					 }];
 	
 	[self.connect setHidden:YES];
+	 */
+	
+//	MCNearbyServiceAdvertiser *advertiser =
+//	[[MCNearbyServiceAdvertiser alloc] initWithPeer:self.localPeerID
+//									  discoveryInfo:nil
+//										serviceType:kServiceType];
+//	advertiser.delegate = self;
+//	[advertiser startAdvertisingPeer];
 }
-
-
 
 #pragma mark - MCBrowserViewControllerDelegate
 
 - (void)browserViewControllerDidFinish:(MCBrowserViewController *)browserViewController
 {
+	[browserViewController.browser stopBrowsingForPeers];
 	[browserViewController dismissViewControllerAnimated:YES
 											  completion:^{
 												  
@@ -83,6 +112,7 @@ static NSString *const kServiceType = @"sapa-textchat";
 
 - (void)browserViewControllerWasCancelled:(MCBrowserViewController *)browserViewController
 {
+	[browserViewController.browser stopBrowsingForPeers];
 	[self.connect setHidden:NO];
 	
 	[browserViewController dismissViewControllerAnimated:YES
@@ -91,7 +121,32 @@ static NSString *const kServiceType = @"sapa-textchat";
 											  }];
 }
 
+- (void)session:(MCSession *)session didReceiveData:(NSData *)data fromPeer:(MCPeerID *)peerID
+{}
 
+- (void)session:(MCSession *)session didReceiveStream:(NSInputStream *)stream withName:(NSString *)streamName fromPeer:(MCPeerID *)peerID
+{}
+
+- (void)session:(MCSession *)session didStartReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID withProgress:(NSProgress *)progress
+{}
+
+- (void)session:(MCSession *)session didFinishReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID atURL:(NSURL *)localURL withError:(NSError *)error
+{}
+
+- (void)session:(MCSession *)session peer:(MCPeerID *)peerID didChangeState:(MCSessionState)state
+{
+	switch (state) {
+		case MCSessionStateConnected:
+			NSLog(@"connected");
+			break;
+		case MCSessionStateNotConnected:
+			NSLog(@"disconnected");
+//			[self closeConnection];
+			break;
+		default:
+			break;
+	}
+}
 
 #pragma mark - MCNearbyServiceBrowserDelegate
 
@@ -103,6 +158,19 @@ static NSString *const kServiceType = @"sapa-textchat";
 - (void)browser:(MCNearbyServiceBrowser *)browser lostPeer:(MCPeerID *)peerID
 {
 	[self.nearestPeers removeObject:peerID];
+}
+
+#pragma mark - MCNearbyServiceAdvertiserDelegate methods
+
+- (void)advertiser:(MCNearbyServiceAdvertiser *)advertiser didNotStartAdvertisingPeer:(NSError *)error
+{
+	NSLog(@"didNotStartAdvertisingPeer: %@", error);
+}
+
+- (void)advertiser:(MCNearbyServiceAdvertiser *)advertiser didReceiveInvitationFromPeer:(MCPeerID *)peerID withContext:(NSData *)context invitationHandler:(void (^)(BOOL, MCSession *))invitationHandler
+{
+	NSLog(@"didReceiveInvitationFromPeer: %@", peerID.displayName);
+	invitationHandler(YES, self.currentSession);
 }
 
 #pragma mark - Navigation
