cc0cfaf13605c41e43d5f79c66330b719ec21c8c
Sapa Denys <sapa.denis@gmail.com>
2015-08-05 00:14:54 +0300

    show and hide keyboard

--- BluetoothChat/BTCConnectViewController.m
+++ BluetoothChat/BTCConnectViewController.m
@@ -18,17 +18,14 @@ static NSString *const kServiceType = @"sapa-textchat";
 
 
 
-@interface BTCConnectViewController () <UITableViewDelegate, UITableViewDataSource, SessionContainerDelegate>
+@interface BTCConnectViewController () <SessionContainerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIButton *sendButton;
 @property (weak, nonatomic) IBOutlet UITextField *messageTextField;
+@property (weak, nonatomic) IBOutlet UITableView *chatTableView;
 
-@property (nonatomic, strong) MCPeerID* localPeerID;
 @property (nonatomic, strong) SessionContainer *sessionContainer;
-
 @property (nonatomic, strong) NSMutableArray *messageContainer;
-
-
 
 @end
 
@@ -53,30 +50,65 @@ static NSString *const kServiceType = @"sapa-textchat";
 
 #pragma mark - UITableViewDataSource
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	if ([tableView isEqual:self.tableView]) {
+		if (indexPath.row == 0) {
+			CGFloat tableViewHeight = self.tableView.frame.size.height;
+			CGFloat statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
+			CGFloat navigationBarHeight = self.navigationController.navigationBar.frame.size.height;
+			return tableViewHeight - statusBarHeight - navigationBarHeight - 50;
+		}
+		
+		if (indexPath.row == 1) {
+			//		CGSize size = [self.messageTextField systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+			return 50;
+		}
+		
+		return [super tableView:tableView heightForRowAtIndexPath:indexPath];
+	}
+	return [super tableView:tableView heightForRowAtIndexPath:indexPath];
+}
 
-//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-//{
-//	return 1;
-//}
-//
-//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-//{
-//	return self.messageContainer.count;
-//}
-//
-//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-//{
-//
-//	Message *message = [self.messageContainer objectAtIndex:indexPath.row];
-//	
-//	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MessageCell" forIndexPath:indexPath];
-//
-//	cell.textLabel.text = message.messageText;
-//	cell.detailTextLabel.text = message.senderName;
-//	
-//	return cell;
-//}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+	return 1;
+}
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	if ([tableView isEqual:self.chatTableView]) {
+		return self.messageContainer.count;
+	} else {
+		return [super tableView:tableView numberOfRowsInSection:section];
+	}
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	if ([tableView isEqual:self.chatTableView]) {
+		Message *message = [self.messageContainer objectAtIndex:indexPath.row];
+		
+		UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MessageCell" forIndexPath:indexPath];
+		
+		cell.textLabel.text = message.messageText;
+		cell.detailTextLabel.text = message.senderName;
+		
+		return cell;
+	} else {
+		return [super tableView:tableView cellForRowAtIndexPath:indexPath];
+	}
+}
+
+- (IBAction)swipeOnMessegeFieldCell:(UIPanGestureRecognizer *)sender
+{
+	CGFloat offsetByY = [sender velocityInView:self.tableView].y;
+	if (offsetByY > 0) {
+		[self.messageTextField resignFirstResponder];
+	} else if (offsetByY < 0) {
+		[self.messageTextField becomeFirstResponder];
+	}
+}
 
 #pragma mark - SessionContainerDelegate
 
