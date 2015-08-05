069ffdfbfed7634919d00b362441d5e9b0d4396c
Sapa Denys <sapa.denis@gmail.com>
2015-08-05 12:11:06 +0300

    refactoring

---
+++ BluetoothChat/MessageMeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MessageMeTableViewCell.h
+//  BluetoothChat
+//
+//  Created by Denis Sapa on 05.08.15.
+//  Copyright (c) 2015 Sapa Denys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageMeTableViewCell : UITableViewCell
+
+@end
---
+++ BluetoothChat/MessageMeTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MessageMeTableViewCell.m
+//  BluetoothChat
+//
+//  Created by Denis Sapa on 05.08.15.
+//  Copyright (c) 2015 Sapa Denys. All rights reserved.
+//
+
+#import "MessageMeTableViewCell.h"
+
+@implementation MessageMeTableViewCell
+
+@end
